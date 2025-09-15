69682930d3b3f6b35eba938ea15e69ccea375334
Hong Sovannara <rapewpew372@gmail.com>
2025-09-15 20:56:31 +0700

    object type

--- public/app.js
+++ public/app.js
@@ -1,9 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+//data type
 let userName = "Hong Sovannara";
 let number = 34;
 let isStudent = true;
 let a; //null it empty
 let b; //undefined it dont know
 console.log(userName);
+//==============================
+// object==========================
+const user = {
+    id: 1,
+    name: "hong sovannara",
+    isStudent: true,
+};
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+//data type
 let userName: string = "Hong Sovannara";
 let number: number = 34;
 let isStudent: boolean = true;
@@ -5,3 +6,12 @@ let isStudent: boolean = true;
 let a: null; //null it empty
 let b: undefined; //undefined it dont know
 console.log(userName);
+
+//==============================
+
+// object==========================
+const user: { id: number; name: string; isStudent: boolean } = {
+  id: 1,
+  name: "hong sovannara",
+  isStudent: true,
+};
