56c4ba46c7b3b73f0201d9ab241664dddf021d27
Hong Sovannara <rapewpew372@gmail.com>
2025-09-15 20:52:11 +0700

    first commit

---
+++ public/app.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let userName = "Hong Sovannara";
+let number = 34;
+let isStudent = true;
+let a; //null it empty
+let b; //undefined it dont know
+console.log(userName);
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,7 @@
+let userName: string = "Hong Sovannara";
+let number: number = 34;
+let isStudent: boolean = true;
+
+let a: null; //null it empty
+let b: undefined; //undefined it dont know
+console.log(userName);
